d96b562b7c3cfd11178a9480557daf95434519c3
trey shanks <shanksleslietrey@gmail.com>
2021-11-18 17:05:33 -0600

    solved sales agent invoice

--- sales_agent_invoices.sql
+++ sales_agent_invoices.sql
@@ -1,3 +1,6 @@
-select FirstName + LastName as FullName
-from Employee
-    
+select e.FirstName, e.LastName, invoiceId
+from Employee e
+    join Customer c
+        on e.EmployeeId = c.supportRepId
+    join Invoice i
+        on i.customerId = c.customerId
