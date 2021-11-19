74afb0df0f3880579274c49c066b0b8b682a279c
trey shanks <shanksleslietrey@gmail.com>
2021-11-19 09:42:25 -0600

    sales per country rounded to 2 decimal places

--- sales_agent_customer_count.sql
+++ sales_agent_customer_count.sql
@@ -1,7 +1,3 @@
-select (e.FirstName ||' '|| e.LastName) as employee_name, count(supportRepId) as number_of_customers
-from Employee e
-    join Customer c
-        on e.EmployeeId = c.supportRepId
-    join Invoice i
-        on i.customerId = c.customerId
-group by employee_name
+select BillingCountry as country, round(sum(total), 2) as sales_per_country
+from Invoice
+group by country
